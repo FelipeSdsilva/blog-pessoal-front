f993f1693d55b9bf26a94f4a8a4c11bfcb370ca8
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-13 12:15:14 -0300

    Inicialização do frontend do projeto blogpessoal

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="blog-nav-content">
+                    <h1></h1>
+                </div>
+            </nav>
+        </header>
+    );
+}
